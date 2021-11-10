0cb0bd451b3ee1d3734bc339848c656fdc8f23dd
rGudavadze <roma.gudavadze@gmail.com>
2021-11-10 18:15:19 +0400

    add review app

---
+++ review/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from user.models import User
+from product.models import Product
+
+
+class Review(models.Model):
+  text = models.TextField()
+  rating = models.IntegerField()
+  user = models.ForeignKey(User, related_name='review', on_delete=models.CASCADE)
+  product = models.ForeignKey(Product, related_name='review', on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Review from {self.user.name} on {self.product.name}"
+  
---
+++ review/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from .views import ReviewViewset
+
+
+router = DefaultRouter()
+router.register('reviews', ReviewViewset, basename='reviews')
+
+
+
+urlpatterns = [
+  url('', include(router.urls))
+]
